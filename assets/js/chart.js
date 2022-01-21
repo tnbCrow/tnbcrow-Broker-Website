9d5dadeb84c59b8f5e3293638a3608259f16e0c7
Mark Bekooy <mark.bekooy@gmail.com>
2022-01-21 19:11:20 +0100

    Consistent styling by Prettier formatter

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,64 +1,57 @@
+$.ajax({
+  url: "https://tnbcrow.pythonanywhere.com/market-chart",
+  success: function (result) {
+    const ctx = document.getElementById("marketChart").getContext("2d");
+    const xlabel = [];
+    const ylabel = [];
 
-    $.ajax({
-        url: "https://tnbcrow.pythonanywhere.com/market-chart", success: function (result) {
-  
-          const ctx = document.getElementById('marketChart').getContext('2d');
-          const xlabel=[];
-          const ylabel=[];
-  
-          result.forEach(element => {
-            xlabel.push(element.created_at)
-            ylabel.push(parseFloat(element.rate)/10000 )
-  
-          });
-  
-          var myChart = new Chart(ctx, {
-            type: 'line',
-            options: {
-              responsive: true,
-              interaction: {
-                  intersect: false,
-                  axis: 'x'
-                  },
-              scales: {
-                x: {
-                  type: 'time',
-                  time: {
-                    minUnit: 'hour'
-                  }
-                },
-                y: {suggestedMin: 0},
-              },
-              plugins: {
-                zoom: {
-                  zoom: {
-                    wheel: {
-                      enabled: true,
-                    },
-                    pinch: {
-                      enabled: true
-                    },
-                    mode: 'xy',
-                  }
-                },
-                
-                
-              },
+    result.forEach((element) => {
+      xlabel.push(element.created_at);
+      ylabel.push(parseFloat(element.rate) / 10000);
+    });
+
+    var myChart = new Chart(ctx, {
+      type: "line",
+      options: {
+        responsive: true,
+        interaction: {
+          intersect: false,
+          axis: "x",
+        },
+        scales: {
+          x: {
+            type: "time",
+            time: {
+              minUnit: "hour",
             },
-            data: {
-              labels: xlabel,
-              datasets: [{
-                label: "TNBC Price",
-                data: ylabel,
-                borderColor: 'rgb(75, 192, 192)',
-                backgroundColor: 'rgba(0, 0, 0)',
-              }]
-            }
-          });
-  
-  
-        }
-      }
-  
-      );
-  
+          },
+          y: { suggestedMin: 0 },
+        },
+        plugins: {
+          zoom: {
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true,
+              },
+              mode: "xy",
+            },
+          },
+        },
+      },
+      data: {
+        labels: xlabel,
+        datasets: [
+          {
+            label: "TNBC Price",
+            data: ylabel,
+            borderColor: "rgb(75, 192, 192)",
+            backgroundColor: "rgba(0, 0, 0)",
+          },
+        ],
+      },
+    });
+  },
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,94 +1,103 @@
-const sellAdvertisementWrap = document.getElementById("sellAdvertisementTable")
-const sellAdvertisementMain = document.getElementById("sellAdvertisementWrap")
+const sellAdvertisementWrap = document.getElementById("sellAdvertisementTable");
+const sellAdvertisementMain = document.getElementById("sellAdvertisementWrap");
 
-const tradesWrap = document.getElementById("trade")
+const tradesWrap = document.getElementById("trade");
 
-const errMsgBuy = document.createElement("td")
-errMsgBuy.colSpan = "3"
-errMsgBuy.innerHTML = "Sorry, there are no buy offers available."
+const errMsgBuy = document.createElement("td");
+errMsgBuy.colSpan = "3";
+errMsgBuy.innerHTML = "Sorry, there are no buy offers available.";
 
-const errMsgSell = document.createElement("td")
-errMsgSell.colSpan = "3"
-errMsgSell.innerHTML = "Sorry, there are no sell offers available."
+const errMsgSell = document.createElement("td");
+errMsgSell.colSpan = "3";
+errMsgSell.innerHTML = "Sorry, there are no sell offers available.";
 
 const apiErr = document.createElement("p");
-apiErr.innerText = "Oh no, seems like the API is not working."
+apiErr.innerText = "Oh no, seems like the API is not working.";
 
 // SELL OFFERS
-fetch("https://tnbcrow-discord-bot.herokuapp.com/advertisement?ordering=price&side=SELL&format=json").then(response => response.json().then(offers =>
-{
-  console.log(offers.count)
-  if (offers.count > 0)
-  {
-    displayAdvertisements(offers, sellAdvertisementWrap)
-  }
-  else
-  {
-    sellWrap.innerHTML = ""
-    sellWrap.appendChild(errMsgSell)
-  }
-})).catch(err =>
-{
-  sellAdvertisementMain.innerHTML = ""
-  sellAdvertisementMain.appendChild(apiErr)
-})
+fetch(
+  "https://tnbcrow-discord-bot.herokuapp.com/advertisement?ordering=price&side=SELL&format=json"
+)
+  .then((response) =>
+    response.json().then((offers) => {
+      console.log(offers.count);
+      if (offers.count > 0) {
+        displayAdvertisements(offers, sellAdvertisementWrap);
+      } else {
+        sellWrap.innerHTML = "";
+        sellWrap.appendChild(errMsgSell);
+      }
+    })
+  )
+  .catch((err) => {
+    sellAdvertisementMain.innerHTML = "";
+    sellAdvertisementMain.appendChild(apiErr);
+  });
 
-function displayAdvertisements(offers, wrapper)
-{
+function displayAdvertisements(offers, wrapper) {
   // let buy_offer_list = []
-  for (let i = 0; i < 8; i++)
-  {
-    let offer = document.createElement('tr');
-    offer.innerHTML = `<td>${offers.results[i].price/100000000}</td><td>${numberWithCommas(offers.results[i].amount/100000000)}</td>`
+  for (let i = 0; i < 8; i++) {
+    let offer = document.createElement("tr");
+    offer.innerHTML = `<td>${
+      offers.results[i].price / 100000000
+    }</td><td>${numberWithCommas(offers.results[i].amount / 100000000)}</td>`;
     wrapper.appendChild(offer);
   }
 }
 
 // Recent Trades
-fetch("https://tnbcrow.pythonanywhere.com/recent-trades").then(res => res.json()).then(trades =>
-{
-  if (trades.count > 0)
-  {
-    displayTrades(trades, tradesWrap)
-  }
-}).catch(err => console.log(err))
+fetch("https://tnbcrow.pythonanywhere.com/recent-trades")
+  .then((res) => res.json())
+  .then((trades) => {
+    if (trades.count > 0) {
+      displayTrades(trades, tradesWrap);
+    }
+  })
+  .catch((err) => console.log(err));
 
-function displayTrades(trades, wrapper)
-{
+function displayTrades(trades, wrapper) {
   // let buy_offer_list = []
-  for (let i = 0; i < trades.count && i < 8; i++)
-  {
-    let trade = document.createElement('tr');
-    trade.innerHTML = `<td>${trades.results[i].rate/10000}</td><td>${trades.results[i].amount}</td><td>${parseInt(trades.results[i].rate/10000 * trades.results[i].amount)}</td>`
+  for (let i = 0; i < trades.count && i < 8; i++) {
+    let trade = document.createElement("tr");
+    trade.innerHTML = `<td>${trades.results[i].rate / 10000}</td><td>${
+      trades.results[i].amount
+    }</td><td>${parseInt(
+      (trades.results[i].rate / 10000) * trades.results[i].amount
+    )}</td>`;
     wrapper.appendChild(trade);
   }
 }
 
-function numberWithCommas(x)
-{
+function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // Statistics
 const rate = document.getElementById("rate");
-const cap = document.getElementById('cap');
-const supply = document.getElementById('circ');
-fetch("https://raw.githubusercontent.com/itsnikhil/tnb-analysis/master/web/js/static.json").then(res => res.json()).then(data =>
-{
-  circulating_supply = data.Total;
-  supply.innerText = numberWithCommas(data.Total);
-}).catch(err =>
-{
-  supply.innerText = "There seems to be something wrong with the API";
-  cap.innerText = "There seems to be something wrong with the API"
-})
+const cap = document.getElementById("cap");
+const supply = document.getElementById("circ");
+fetch(
+  "https://raw.githubusercontent.com/itsnikhil/tnb-analysis/master/web/js/static.json"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    circulating_supply = data.Total;
+    supply.innerText = numberWithCommas(data.Total);
+  })
+  .catch((err) => {
+    supply.innerText = "There seems to be something wrong with the API";
+    cap.innerText = "There seems to be something wrong with the API";
+  });
 
-fetch("https://tnbcrow.pythonanywhere.com/recent-trades").then(res => res.json()).then(data =>
-{
-  rate.innerText = data.results[0].rate / 10000
-  cap.innerText = numberWithCommas(parseInt(circulating_supply * rate.innerText));
-}).catch(err =>
-{
-  rate.innerText = "There seems to be something wrong with the API"
-  cap.innerText = "There seems to be something wrong with the API"
-})
+fetch("https://tnbcrow.pythonanywhere.com/recent-trades")
+  .then((res) => res.json())
+  .then((data) => {
+    rate.innerText = data.results[0].rate / 10000;
+    cap.innerText = numberWithCommas(
+      parseInt(circulating_supply * rate.innerText)
+    );
+  })
+  .catch((err) => {
+    rate.innerText = "There seems to be something wrong with the API";
+    cap.innerText = "There seems to be something wrong with the API";
+  });
